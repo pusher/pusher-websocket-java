e77d05210d99b0ae735cad9dc681059d72c30704
Adam Shone <adam.shone@gmail.com>
2012-12-27 11:23:48 +0000

    Added the Java-WebSocket library as a Git submodule. This could be changed to a Maven dependency once the websocket library has been uploaded to a public repo - see https://github.com/TooTallNate/Java-WebSocket/issues/118

---
+++ src/main/java/com/pusher/client/connection/WebsocketConnection.java
@@ -0,0 +1,23 @@
+package com.pusher.client.connection;
+
+public class WebsocketConnection implements Connection {
+
+    @Override
+    public void connect(String apiKey) {
+	
+    }
+
+    @Override
+    public void setEventListener(ConnectionEventListener eventListener) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void bind(ConnectionState state,
+	    ConnectionEventListener eventListener) {
+	// TODO Auto-generated method stub
+	
+    }
+
+}
