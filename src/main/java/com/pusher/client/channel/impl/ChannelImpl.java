fd5bdf1bbcec2dc295b274a28c46b506795074aa
Phil Leggetter <phil@leggetter.co.uk>
2013-05-22 22:15:25 +0100

    Formatting.

--- src/main/java/com/pusher/client/channel/impl/ChannelImpl.java
+++ src/main/java/com/pusher/client/channel/impl/ChannelImpl.java
@@ -14,129 +14,136 @@ import com.pusher.client.util.Factory;
 
 public class ChannelImpl implements InternalChannel {
 
-    private static final String INTERNAL_EVENT_PREFIX = "pusher_internal:";
-    protected static final String SUBSCRIPTION_SUCCESS_EVENT = "pusher_internal:subscription_succeeded";
-    protected final String name;
-    protected final Map<String, Set<SubscriptionEventListener>> eventNameToListenerMap = new HashMap<String, Set<SubscriptionEventListener>>();
-    protected ChannelState state = ChannelState.INITIAL;
+	private static final String INTERNAL_EVENT_PREFIX = "pusher_internal:";
+	protected static final String SUBSCRIPTION_SUCCESS_EVENT = "pusher_internal:subscription_succeeded";
+	protected final String name;
+	protected final Map<String, Set<SubscriptionEventListener>> eventNameToListenerMap = new HashMap<String, Set<SubscriptionEventListener>>();
+	protected ChannelState state = ChannelState.INITIAL;
 	private ChannelEventListener eventListener;
 
-    public ChannelImpl(String channelName) {
-	
-	if(channelName == null) {
-	    throw new IllegalArgumentException("Cannot subscribe to a channel with a null name");
-	}
-	
-	for(String disallowedPattern : getDisallowedNameExpressions()) {
-	    if(channelName.matches(disallowedPattern)) {
-		throw new IllegalArgumentException("Channel name " + channelName + " is invalid. Private channel names must start with \"private-\" and presence channel names must start with \"presence-\"");
-	    }
-	}
-	
-	this.name = channelName;
-    }
+	public ChannelImpl(String channelName) {
 
-    /* Channel implementation */
-    
-    @Override
-    public String getName() {
-	return name;
-    }
-    
-    @Override
-    public void bind(String eventName, SubscriptionEventListener listener) {
-	
-	validateArguments(eventName, listener);
-	
-	Set<SubscriptionEventListener> listeners = eventNameToListenerMap.get(eventName);
-	if(listeners == null) {
-	    listeners = new HashSet<SubscriptionEventListener>();
-	    eventNameToListenerMap.put(eventName, listeners);
-	}
-	
-	listeners.add(listener);
-    }
-
-    @Override
-    public void unbind(String eventName, SubscriptionEventListener listener) {
-	
-	validateArguments(eventName, listener);
-	
-	Set<SubscriptionEventListener> listeners = eventNameToListenerMap.get(eventName);
-	if(listeners != null) {
-	    listeners.remove(listener);
-	    if(listeners.isEmpty()) {
-		eventNameToListenerMap.remove(eventName);
-	    }
-	}
-    }
-    
-    /* InternalChannel implementation */
-   
-    @Override
-    public void onMessage(final String event, String message) {
-
-	if(event.equals(SUBSCRIPTION_SUCCESS_EVENT)) {
-	    updateState(ChannelState.SUBSCRIBED);
-	} else {
-	    Set<SubscriptionEventListener> listeners = eventNameToListenerMap.get(event);
-	    if(listeners != null) {
-		for(final SubscriptionEventListener listener : listeners) {
-        		
-		    final String data = extractDataFrom(message);
-        		
-		    Factory.getEventQueue().execute(new Runnable() {
-			public void run() {
-			    listener.onEvent(name, event, data);
-			}
-		    });
+		if (channelName == null) {
+			throw new IllegalArgumentException(
+					"Cannot subscribe to a channel with a null name");
 		}
-	    }
-	}
-    }
-    
-    @Override
-    public String toSubscribeMessage() {
-	
-	Map<Object, Object> jsonObject = new LinkedHashMap<Object, Object>();
-	jsonObject.put("event", "pusher:subscribe");
-	
-	Map<Object, Object> dataMap = new LinkedHashMap<Object, Object>();
-	dataMap.put("channel", name);
-	
-	jsonObject.put("data", dataMap);
-	
-	return new Gson().toJson(jsonObject);
-    }
-    
-    @Override
-    public String toUnsubscribeMessage() {
-	Map<Object, Object> jsonObject = new LinkedHashMap<Object, Object>();
-	jsonObject.put("event", "pusher:unsubscribe");
-	
-	Map<Object, Object> dataMap = new LinkedHashMap<Object, Object>();
-	dataMap.put("channel", name);
-	
-	jsonObject.put("data", dataMap);
-	
-	return new Gson().toJson(jsonObject);
-    }
 
-    @Override
-    public void updateState(ChannelState state) {
-	
-	this.state = state;
-	
-	if(state == ChannelState.SUBSCRIBED && eventListener != null) {
-	    Factory.getEventQueue().execute(new Runnable() {
-			public void run() {
-			    eventListener.onSubscriptionSucceeded(ChannelImpl.this.getName());
+		for (String disallowedPattern : getDisallowedNameExpressions()) {
+			if (channelName.matches(disallowedPattern)) {
+				throw new IllegalArgumentException(
+						"Channel name "
+								+ channelName
+								+ " is invalid. Private channel names must start with \"private-\" and presence channel names must start with \"presence-\"");
 			}
-		    });
 		}
-    }
 
-    /* Comparable implementation */
+		this.name = channelName;
+	}
+
+	/* Channel implementation */
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void bind(String eventName, SubscriptionEventListener listener) {
+
+		validateArguments(eventName, listener);
+
+		Set<SubscriptionEventListener> listeners = eventNameToListenerMap
+				.get(eventName);
+		if (listeners == null) {
+			listeners = new HashSet<SubscriptionEventListener>();
+			eventNameToListenerMap.put(eventName, listeners);
+		}
+
+		listeners.add(listener);
+	}
+
+	@Override
+	public void unbind(String eventName, SubscriptionEventListener listener) {
+
+		validateArguments(eventName, listener);
+
+		Set<SubscriptionEventListener> listeners = eventNameToListenerMap
+				.get(eventName);
+		if (listeners != null) {
+			listeners.remove(listener);
+			if (listeners.isEmpty()) {
+				eventNameToListenerMap.remove(eventName);
+			}
+		}
+	}
+
+	/* InternalChannel implementation */
+
+	@Override
+	public void onMessage(final String event, String message) {
+
+		if (event.equals(SUBSCRIPTION_SUCCESS_EVENT)) {
+			updateState(ChannelState.SUBSCRIBED);
+		} else {
+			Set<SubscriptionEventListener> listeners = eventNameToListenerMap
+					.get(event);
+			if (listeners != null) {
+				for (final SubscriptionEventListener listener : listeners) {
+
+					final String data = extractDataFrom(message);
+
+					Factory.getEventQueue().execute(new Runnable() {
+						public void run() {
+							listener.onEvent(name, event, data);
+						}
+					});
+				}
+			}
+		}
+	}
+
+	@Override
+	public String toSubscribeMessage() {
+
+		Map<Object, Object> jsonObject = new LinkedHashMap<Object, Object>();
+		jsonObject.put("event", "pusher:subscribe");
+
+		Map<Object, Object> dataMap = new LinkedHashMap<Object, Object>();
+		dataMap.put("channel", name);
+
+		jsonObject.put("data", dataMap);
+
+		return new Gson().toJson(jsonObject);
+	}
+
+	@Override
+	public String toUnsubscribeMessage() {
+		Map<Object, Object> jsonObject = new LinkedHashMap<Object, Object>();
+		jsonObject.put("event", "pusher:unsubscribe");
+
+		Map<Object, Object> dataMap = new LinkedHashMap<Object, Object>();
+		dataMap.put("channel", name);
+
+		jsonObject.put("data", dataMap);
+
+		return new Gson().toJson(jsonObject);
+	}
+
+	@Override
+	public void updateState(ChannelState state) {
+
+		this.state = state;
+
+		if (state == ChannelState.SUBSCRIBED && eventListener != null) {
+			Factory.getEventQueue().execute(new Runnable() {
+				public void run() {
+					eventListener.onSubscriptionSucceeded(ChannelImpl.this.getName());
+				}
+			});
+		}
+	}
+
+	/* Comparable implementation */
 
 	@Override
 	public void setEventListener(ChannelEventListener listener) {
@@ -147,49 +154,51 @@ public class ChannelImpl implements InternalChannel {
 	public ChannelEventListener getEventListener() {
 		return eventListener;
 	}
-    
-    @Override
-    public int compareTo(InternalChannel other) {
-	return this.getName().compareTo(other.getName());
-    }
-    
-    /* implementation detail */
-    
-    @Override
-    public String toString() {
-	return String.format("[Public Channel: name=%s]", name);
-    }
-    
-    @SuppressWarnings("unchecked")
-    private String extractDataFrom(String message) {
-	Gson gson = new Gson();
-	Map<Object, Object> jsonObject = gson.fromJson(message, Map.class);
-	return (String)jsonObject.get("data");
-    }
 
-    protected String[] getDisallowedNameExpressions() {
-	return new String[] {
-		"^private-.*",
-		"^presence-.*"
-	};
-    }
-    
-    private void validateArguments(String eventName, SubscriptionEventListener listener) {
-	
-	if(eventName == null) {
-	    throw new IllegalArgumentException("Cannot bind or unbind to channel " + name + " with a null event name");
+	@Override
+	public int compareTo(InternalChannel other) {
+		return this.getName().compareTo(other.getName());
 	}
-	
-	if(listener == null) {
-	    throw new IllegalArgumentException("Cannot bind or unbind to channel " + name + " with a null listener");
+
+	/* implementation detail */
+
+	@Override
+	public String toString() {
+		return String.format("[Public Channel: name=%s]", name);
 	}
-	
-	if(eventName.startsWith(INTERNAL_EVENT_PREFIX)) {
-	    throw new IllegalArgumentException("Cannot bind or unbind channel " + name + " with an internal event name such as " + eventName);
+
+	@SuppressWarnings("unchecked")
+	private String extractDataFrom(String message) {
+		Gson gson = new Gson();
+		Map<Object, Object> jsonObject = gson.fromJson(message, Map.class);
+		return (String) jsonObject.get("data");
 	}
-	
-	if(state == ChannelState.UNSUBSCRIBED) {
-	    throw new IllegalStateException("Cannot bind or unbind to events on a channel that has been unsubscribed. Call Pusher.subscribe() to resubscribe to this channel");
+
+	protected String[] getDisallowedNameExpressions() {
+		return new String[] { "^private-.*", "^presence-.*" };
 	}
-    }
+
+	private void validateArguments(String eventName,
+			SubscriptionEventListener listener) {
+
+		if (eventName == null) {
+			throw new IllegalArgumentException("Cannot bind or unbind to channel "
+					+ name + " with a null event name");
+		}
+
+		if (listener == null) {
+			throw new IllegalArgumentException("Cannot bind or unbind to channel "
+					+ name + " with a null listener");
+		}
+
+		if (eventName.startsWith(INTERNAL_EVENT_PREFIX)) {
+			throw new IllegalArgumentException("Cannot bind or unbind channel "
+					+ name + " with an internal event name such as " + eventName);
+		}
+
+		if (state == ChannelState.UNSUBSCRIBED) {
+			throw new IllegalStateException(
+					"Cannot bind or unbind to events on a channel that has been unsubscribed. Call Pusher.subscribe() to resubscribe to this channel");
+		}
+	}
 }
