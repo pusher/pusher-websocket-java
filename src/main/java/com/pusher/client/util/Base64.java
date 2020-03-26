1d43fdba6750a6af6907b2c2c20aa94717132e19
Marek Urbaniak <8502071+marekoid@users.noreply.github.com>
2020-03-26 16:03:37 +0000

    Use a better term
    As it's not a temporary store for I/O.

--- src/main/java/com/pusher/client/util/Base64.java
+++ src/main/java/com/pusher/client/util/Base64.java
@@ -25,24 +25,24 @@ public class Base64 {
 
     public static byte[] decode(String base64String) {
         int paddingSize = base64String.endsWith("==") ? 2 : base64String.endsWith("=") ? 1 : 0;
-        byte[] buffer = new byte[base64String.length() * 3 / 4 - paddingSize];
+        byte[] retVal = new byte[base64String.length() * 3 / 4 - paddingSize];
         int mask = 0xFF;
         int index = 0;
         for (int i = 0; i < base64String.length(); i += 4) {
             int c0 = toInt(base64String.charAt(i));
             int c1 = toInt(base64String.charAt(i + 1));
-            buffer[index++] = (byte) (((c0 << 2) | (c1 >> 4)) & mask);
-            if (index >= buffer.length) {
-                return buffer;
+            retVal[index++] = (byte) (((c0 << 2) | (c1 >> 4)) & mask);
+            if (index >= retVal.length) {
+                return retVal;
             }
             int c2 = toInt(base64String.charAt(i + 2));
-            buffer[index++] = (byte) (((c1 << 4) | (c2 >> 2)) & mask);
-            if (index >= buffer.length) {
-                return buffer;
+            retVal[index++] = (byte) (((c1 << 4) | (c2 >> 2)) & mask);
+            if (index >= retVal.length) {
+                return retVal;
             }
             int c3 = toInt(base64String.charAt(i + 3));
-            buffer[index++] = (byte) (((c2 << 6) | c3) & mask);
+            retVal[index++] = (byte) (((c2 << 6) | c3) & mask);
         }
-        return buffer;
+        return retVal;
     }
 }
