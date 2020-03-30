d5bfb2e937eb6c671df916c9cfb92f86ea39e37e
Danielle Vass <danielle.vass@pusher.com>
2020-03-30 11:42:20 +0100

    Refactor PrivateEncryptedChannelImplTest

--- src/test/java/com/pusher/client/channel/impl/PrivateEncryptedChannelImplTest.java
+++ src/test/java/com/pusher/client/channel/impl/PrivateEncryptedChannelImplTest.java
@@ -5,44 +5,39 @@ import com.pusher.client.Authorizer;
 import com.pusher.client.connection.impl.InternalConnection;
 import com.pusher.client.util.Factory;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PrivateEncryptedChannelImplTest {
 
-    private String authorizer_valid = "{\"auth\":\"636a81ba7e7b15725c00:3ee04892514e8a669dc5d30267221f16727596688894712cad305986e6fc0f3c\",\"shared_secret\":\"iBvNoPVYwByqSfg6anjPpEQ2j051b3rt1Vmnb+z5doo=\"}";
-    private String authorizer_missingAuthKey = "{\"shared_secret\":\"iBvNoPVYwByqSfg6anjPpEQ2j051b3rt1Vmnb+z5doo=\"}";
-    private String authorizer_missingSharedSecret = "{\"auth\":\"636a81ba7e7b15725c00:3ee04892514e8a669dc5d30267221f16727596688894712cad305986e6fc0f3c\"}";
-    private String authorizer_malformedJson = "potatoes";
+    String authorizer_valid = "{\"auth\":\"636a81ba7e7b15725c00:3ee04892514e8a669dc5d30267221f16727596688894712cad305986e6fc0f3c\",\"shared_secret\":\"iBvNoPVYwByqSfg6anjPpEQ2j051b3rt1Vmnb+z5doo=\"}";
+    String authorizer_missingAuthKey = "{\"shared_secret\":\"iBvNoPVYwByqSfg6anjPpEQ2j051b3rt1Vmnb+z5doo=\"}";
+    String authorizer_missingSharedSecret = "{\"auth\":\"636a81ba7e7b15725c00:3ee04892514e8a669dc5d30267221f16727596688894712cad305986e6fc0f3c\"}";
+    String authorizer_malformedJson = "potatoes";
 
-    private InternalConnection internalConnection;
-    private Authorizer authorizer;
-    private Factory factory;
-
-    @Before
-    public void setup() {
-        internalConnection = mock(InternalConnection.class);
-        authorizer = mock(Authorizer.class);
-        factory = mock(Factory.class);
-    }
+    @Mock
+    InternalConnection mockedInternalConnection;
+    @Mock
+    Authorizer mockedAuthorizer;
+    @Mock
+    Factory mockedFactory;
 
     @Test
     public void testPrepareThrowsNoExceptionIfValidAuthorizer() {
-        when(authorizer.authorize(Matchers.anyString(), Matchers.anyString()))
+        when(mockedAuthorizer.authorize(Matchers.anyString(), Matchers.anyString()))
                 .thenReturn(authorizer_valid);
 
         PrivateEncryptedChannelImpl channel = new PrivateEncryptedChannelImpl(
-                internalConnection, "private-encrypted-channel", authorizer, factory);
+                mockedInternalConnection, "private-encrypted-channel", mockedAuthorizer, mockedFactory);
 
         Exception exception = null;
         try {
@@ -56,11 +51,11 @@ public class PrivateEncryptedChannelImplTest {
 
     @Test
     public void testPrepareThrowsExceptionIfNoAuthKey() {
-        when(authorizer.authorize(Matchers.anyString(), Matchers.anyString()))
+        when(mockedAuthorizer.authorize(Matchers.anyString(), Matchers.anyString()))
                 .thenReturn(authorizer_missingAuthKey);
 
         PrivateEncryptedChannelImpl channel = new PrivateEncryptedChannelImpl(
-                internalConnection, "private-encrypted-channel", authorizer, factory);
+                mockedInternalConnection, "private-encrypted-channel", mockedAuthorizer, mockedFactory);
 
         Exception exception = null;
         try {
@@ -79,11 +74,11 @@ public class PrivateEncryptedChannelImplTest {
 
     @Test
     public void testPrepareThrowsExceptionIfNoSharedSecret() {
-        when(authorizer.authorize(Matchers.anyString(), Matchers.anyString()))
+        when(mockedAuthorizer.authorize(Matchers.anyString(), Matchers.anyString()))
                 .thenReturn(authorizer_missingSharedSecret);
 
         PrivateEncryptedChannelImpl channel = new PrivateEncryptedChannelImpl(
-                internalConnection, "private-encrypted-channel", authorizer, factory);
+                mockedInternalConnection, "private-encrypted-channel", mockedAuthorizer, mockedFactory);
 
         Exception exception = null;
         try {
@@ -101,11 +96,11 @@ public class PrivateEncryptedChannelImplTest {
 
     @Test
     public void testPrepareThrowsExceptionIfMalformedJson() {
-        when(authorizer.authorize(Matchers.anyString(), Matchers.anyString()))
+        when(mockedAuthorizer.authorize(Matchers.anyString(), Matchers.anyString()))
                 .thenReturn(authorizer_malformedJson);
 
         PrivateEncryptedChannelImpl channel = new PrivateEncryptedChannelImpl(
-                internalConnection, "private-encrypted-channel", authorizer, factory);
+                mockedInternalConnection, "private-encrypted-channel", mockedAuthorizer, mockedFactory);
 
         Exception exception = null;
         try {
