e311d5502a6a1682002b1d77067b3b981fdcdf27
Danielle Vass <danielle.vass@pusher.com>
2020-04-08 12:06:08 +0100

    Update comment on PrivateEncryptedChannelEventListener

--- src/main/java/com/pusher/client/channel/PrivateEncryptedChannelEventListener.java
+++ src/main/java/com/pusher/client/channel/PrivateEncryptedChannelEventListener.java
@@ -2,7 +2,9 @@ package com.pusher.client.channel;
 
 /**
  * Interface to listen to private encrypted channel events.
- * Note: This needs to extend the PrivateChannelEventListener because of the ChannelManager clearDownSubscription
+ * Note: This needs to extend the PrivateChannelEventListener because in the
+ * ChannelManager handleAuthenticationFailure we assume it's safe to cast to a
+ * PrivateChannelEventListener
  */
 public interface PrivateEncryptedChannelEventListener extends PrivateChannelEventListener {
 
